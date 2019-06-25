8dea2c3e2040af054e6a5625a8c771cdfb79e72d
JackSnowdon <jacksnowdondrums@gmail.com>
2019-06-25 18:01:39 +0000

    added stripe js and wired up checkout

---
+++ static/js/stripe.js
@@ -0,0 +1,33 @@
+$(function() {
+    $("#payment-form").sumbit(function() {
+        var form = this;
+        var card = {
+            number: $("#id_credit_card_number").val(),
+            expMonth: $("#id_exipry_month").val(),
+            expYear: $("#id_exipry_year").val(),
+            cvc: $("#id_cvv").val()
+        };
+
+        Stripe.createToken(card, function(status, response) {
+            if (status === 200) {
+                $("#credit-card-errors").hide();
+                $("#id_stripe_id").val(response.id);
+
+                //Prevent the Credit card Details from being submitted to our server 
+                $("#id_credit_card_number").removeAttr('name');
+                $("#id_cvv").removeAttr('name');
+                $("#id_exipry_month").removeAttr('name');
+                $("#id_exipry_year").removeAttr('name');
+                
+                form.sumbit();
+                
+                
+            } else {
+                $("#stripe-error-message").text(response.error.message);
+                $("#credit-card-errors").show();
+                $("#validate_card_btn").attr("disabled", false);
+            }
+        });
+        return false;
+    });
+});
